8acc6e94044cdd993b644d7c66eff5ab7738b53e
Rohitkumarvarma-369 <79484529+Rohitkumarvarma-369@users.noreply.github.com>
2022-03-22 01:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def get_predict(data: carrate):
 
 # Configuring the server host and port
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='127.0.0.1')
+    uvicorn.run(app, port=5000, host='0.0.0.0')
 
 
 
